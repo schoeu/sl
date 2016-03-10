b25584d5fd9b449316ec0b08694b9d1c9dbf3dd4
schoeu <quanyou1013@gmail.com>
2016-03-10 11:05:39 +0800

    edit extend

--- src/sLib.js
+++ src/sLib.js
@@ -43,312 +43,6 @@
         return this;
     };
 
-    $.fn.hide = function(){
-        this.each(function(){
-            this.style.display = 'none';
-        });
-    };
-
-    $.fn.each = function(fn){
-        var i= 0,lth = this.length;
-        for(;i<lth;i++){
-            fn.call(this[i],i,this[i]);
-        }
-
-    };
-
-    $.fn.prePend = function(newNode){
-        var parentNode = this[0].parentNode,
-            parNode = parentNode.firstChild;
-        if(parNode){
-            parentNode.insertBefore(newNode,parNode);
-        }else{
-            parentNode.appendChild(newNode);
-        }
-    };
-
-    $.fn.addClass = function(cls){
-        this.each(function(){
-            var clsName = this.className,
-                reg = new RegExp("\\b"+cls+"\\b");
-            if(!clsName.match(reg)){
-                this.className += " "+cls;
-            }
-        });
-    };
-
-    $.fn.removeClass = function(cls){
-        this.each(function(){
-            var clsName = el.className,
-                reg = new RegExp("\\b"+cls+"\\b","g");
-            this.className = clsName.replace(reg,"");
-        });
-    };
-
-    $.fn.children = function(tagName){
-        var parentNode = this[0],
-            children = parentNode.children,arr = [];
-        for(var i= 0,lth = children.length;i<lth;i++){
-            var c = children[i]
-            if(!tagName && c.nodeType == 1){
-                arr.push(c);
-            }else if(c.tagName == tagName.toUpperCase()){
-                arr.push(c);
-            }
-        }
-        return arr;
-    };
-
-    $.fn.setCss = function(attr,val){
-        this.each(function(){
-            if(val){
-                try{
-                    this.style[attr] = val;
-                }catch(e){}
-            }
-        });
-    };
-
-    $.fn.hasCLass = function(cls){
-        var el = this[0];
-        if(arguments.length<1){
-            throw new Error("arguments error");
-        }
-        var className = el.className,
-            reg = new RegExp("\\b"+cls+"\\b","g");
-        if(reg.test(className)){
-            return true;
-        }
-        return false;
-    };
-
-    $.fn.isLast = function(){
-        var el = this[0];
-        if(!this.nextEle(el)){
-            return true;
-        }
-        return false;
-    };
-
-    $.fn.isFirst = function(){
-        var el = this[0];
-        if(!this.preEle(el)){
-            return true;
-        }
-        return false;
-    };
-
-    $.fn.closet = function(){
-        var el = this[0],
-            a = [], p,n;
-        p = this.preEle(el);
-        n = this.nextEle(el);
-        if(p){a.push(p);}
-        if(n){a.push(n);}
-        return a;
-    };
-
-    $.fn.getIndex = function(){
-        var el = this[0],
-            pre = this.preSiblings(el);
-        if(pre){
-            return pre.length;
-        }
-        return null;
-    };
-
-    $.fn.nextEle = function(){
-        var el = this[0];
-        if(typeof el.nextElementSibling == "object"){
-            return el.nextElementSibling;
-        }else{
-            var ne = el.nextSibling;
-            while(ne){
-                if(ne.nodeType == 1){
-                    return ne;
-                }
-                ne = ne.nextSibling;
-            }
-        }
-    };
-
-    $.fn.preEle = function(el){
-        //var el = this[0];
-        if(typeof el.previousElementSibling == "object"){
-            return el.previousElementSibling;
-        }else{
-            var pe = el.previousSibling;
-            while(pe){
-                if(pe.nodeType == 1){
-                    return pe;
-                }
-                pe = pe.previousSibling;
-            }
-        }
-    };
-
-    $.fn.nextSiblings = function(){
-        var el = this[0];
-        if(!el) return [];
-        var a = [], nxt = el.nextEle();
-        while(nxt){
-            a.push(nxt);
-            nxt = nxt.nextEle();
-        }
-        return a;
-    };
-
-    $.fn.preSiblings = function(){
-        var el = this[0],a = [];
-        if(!el) return [];
-        el = this.preEle(el);
-        while(el){
-            a.unshift(el);
-            el = this.preEle(el);
-        }
-        return a;
-    };
-
-    $.fn.getCss = function(attr){
-        var el = this[0];
-        try{
-            return window.getComputedStyle(el,null)[attr];//ie8+ w3c
-        }catch(e){
-            return el.currentStyle(attr);
-        }
-    };
-
-    $.fn.css = function(attr,val){
-        var para = arguments.length,el = this[0];
-        if(para == 2){
-            this.each(function(){
-                try{
-                    if(val){
-                        this.style[attr] = val;
-                    }
-                }catch(e){
-                    console.log(e.message);
-                }
-            });
-        }else if(para == 1){
-            if(typeof attr == "object"){
-                for(var a in attr){
-                    this.setCss(el,a,attr[a]);
-                }
-            }else if(typeof attr == "string"){
-                try{
-                    return window.getComputedStyle(el,null)[attr];//ie8+,w3c
-                }catch(e){
-                    return el.currentStyle(attr);
-                }
-            }
-        }
-    };
-
-    $.fn.insertAfter = function(newNode){
-        var el = this[0];
-        if(el.nextElementSibling){
-            el.parentNode.insertBefore(newNode,el.nextElementSibling);
-        }else{
-            el.parentNode.insertBefore(newNode);
-        }
-    };
-
-    $.fn.siblings = function(){
-        var el = this[0];
-        if(el){
-            return this.preSiblings(el).concat(this.nextSiblings(el));
-        }
-        return [];
-    };
-
-    $.fn.offset = function(){
-        var el = this[0],
-            l = el.offsetLeft || 0,
-            t = el.offsetTop || 0,
-            p = el.offsetParent;
-        while(p){
-            l += p.offsetLeft;
-            t += p.offsetTop;
-            p = p.offsetParent
-        }
-        return {left:l,top:t};
-    };
-
-    //simple event bind
-    $.fn.addEvent = function(type,fn){
-        var el = this[0];
-        if(el.addEventListener){
-            el.addEventListener(type,fn,false);
-        }else if(el.attachEvent){
-            el.attachEvent("on"+type,fn);
-        }
-    };
-
-    $.fn.text = function(text){
-        this.each(function(){
-            this.innerHTML = text;
-        });
-    };
-
-    $.fn.removeEvent = function(type,fn){
-        var el = this[0];
-        if(el.removeEventListener){
-            el.removeEventListener(type,fn,false);
-        }else if(el.detachEvent){
-            el.detachEvent("on"+type,fn);
-        }
-    };
-
-    $.fn.bind = function(type,fn){
-        this.each(function(){
-            if(this.addEventListener){
-                this.addEventListener(type,fn,false);
-            }else if(this.attachEvent){
-                this.attachEvent("on"+type,fn);
-            }
-        });
-    };
-
-    $.fn.unbind = function(type,fn){
-        this.each(function(){
-            if(this.removeEventListener){
-                this.removeEventListener(type,fn,false);
-            }else if(this.detachEvent){
-                this.detachEvent("on"+type,fn);
-            }
-        });
-    };
-
-
-    // merge 函数
-    $.extend = function () {
-        var idx = 0;
-        var deep = false;
-        var temp = {};
-        var tar = arguments[idx] || {};
-        if (typeof arguments[idx] === 'boolean') {
-            tar = arguments[++idx];
-            deep = arguments[idx];
-        }
-
-        while(temp = arguments[++idx]) {
-            if ($.isPlainObject(temp)) {
-                for (var i in temp) {
-                    var item = temp[i];
-                    if (deep) {
-                        tar[i] = item;
-                    }
-                    else if (tar[i] === undefined){
-                        tar[i] = item;
-                    }
-                }
-            }
-        }
-        return tar;
-    };
-
-
     /********  instance funciton  end*********/
 
     if(typeof [].distinct === 'undefined'){
@@ -658,6 +352,287 @@
             xhr.send(null);
         }
     };
+
+
+    // merge 函数
+    $.extend = function () {
+        var idx = 0;
+        var deep = false;
+        var temp = {};
+        var tar = arguments[idx] || {};
+        if (typeof arguments[idx] === 'boolean') {
+            tar = arguments[++idx];
+            deep = arguments[idx];
+        }
+
+        while(temp = arguments[++idx]) {
+            if ($.isPlainObject(temp)) {
+                for (var i in temp) {
+                    var item = temp[i];
+                    if (deep) {
+                        tar[i] = item;
+                    }
+                    else if (tar[i] === undefined){
+                        tar[i] = item;
+                    }
+                }
+            }
+        }
+        return tar;
+    };
+
+
+    $.extend($.fn, {
+        hide: function(){
+            this.each(function(){
+                this.style.display = 'none';
+            });
+        },
+        each: function(fn){
+            var i= 0,lth = this.length;
+            for(;i<lth;i++){
+                fn.call(this[i],i,this[i]);
+            }
+        },
+        prePend: function(newNode){
+            var parentNode = this[0].parentNode,
+                parNode = parentNode.firstChild;
+            if(parNode){
+                parentNode.insertBefore(newNode,parNode);
+            }else{
+                parentNode.appendChild(newNode);
+            }
+        },
+        addClass: function(cls){
+            this.each(function(){
+                var clsName = this.className,
+                    reg = new RegExp("\\b"+cls+"\\b");
+                if(!clsName.match(reg)){
+                    this.className += " "+cls;
+                }
+            });
+        },
+        removeClass: function(cls){
+            this.each(function(){
+                var clsName = el.className,
+                    reg = new RegExp("\\b"+cls+"\\b","g");
+                this.className = clsName.replace(reg,"");
+            });
+        },
+        children: function(tagName){
+            var parentNode = this[0],
+                children = parentNode.children,arr = [];
+            for(var i= 0,lth = children.length;i<lth;i++){
+                var c = children[i]
+                if(!tagName && c.nodeType == 1){
+                    arr.push(c);
+                }else if(c.tagName == tagName.toUpperCase()){
+                    arr.push(c);
+                }
+            }
+            return arr;
+        },
+        setCss: function(attr,val){
+            this.each(function(){
+                if(val){
+                    try{
+                        this.style[attr] = val;
+                    }catch(e){}
+                }
+            });
+        },
+        hasCLass: function(cls){
+            var el = this[0];
+            if(arguments.length<1){
+                throw new Error("arguments error");
+            }
+            var className = el.className,
+                reg = new RegExp("\\b"+cls+"\\b","g");
+            if(reg.test(className)){
+                return true;
+            }
+            return false;
+        },
+        isLast: function(){
+            var el = this[0];
+            if(!this.nextEle(el)){
+                return true;
+            }
+            return false;
+        },
+        isFirst: function(){
+            var el = this[0];
+            if(!this.preEle(el)){
+                return true;
+            }
+            return false;
+        },
+        closet: function(){
+            var el = this[0],
+                a = [], p,n;
+            p = this.preEle(el);
+            n = this.nextEle(el);
+            if(p){a.push(p);}
+            if(n){a.push(n);}
+            return a;
+        },
+        getIndex: function(){
+            var el = this[0],
+                pre = this.preSiblings(el);
+            if(pre){
+                return pre.length;
+            }
+            return null;
+        },
+        nextEle: function(){
+            var el = this[0];
+            if(typeof el.nextElementSibling == "object"){
+                return el.nextElementSibling;
+            }else{
+                var ne = el.nextSibling;
+                while(ne){
+                    if(ne.nodeType == 1){
+                        return ne;
+                    }
+                    ne = ne.nextSibling;
+                }
+            }
+        },
+        preEle: function(el){
+            //var el = this[0];
+            if(typeof el.previousElementSibling == "object"){
+                return el.previousElementSibling;
+            }else{
+                var pe = el.previousSibling;
+                while(pe){
+                    if(pe.nodeType == 1){
+                        return pe;
+                    }
+                    pe = pe.previousSibling;
+                }
+            }
+        },
+        nextSiblings: function(){
+            var el = this[0];
+            if(!el) return [];
+            var a = [], nxt = el.nextEle();
+            while(nxt){
+                a.push(nxt);
+                nxt = nxt.nextEle();
+            }
+            return a;
+        },
+        preSiblings: function(){
+            var el = this[0],a = [];
+            if(!el) return [];
+            el = this.preEle(el);
+            while(el){
+                a.unshift(el);
+                el = this.preEle(el);
+            }
+            return a;
+        },
+        getCss: function(attr){
+            var el = this[0];
+            try{
+                return window.getComputedStyle(el,null)[attr];//ie8+ w3c
+            }catch(e){
+                return el.currentStyle(attr);
+            }
+        },
+        css: function(attr,val){
+            var para = arguments.length,el = this[0];
+            if(para == 2){
+                this.each(function(){
+                    try{
+                        if(val){
+                            this.style[attr] = val;
+                        }
+                    }catch(e){
+                        console.log(e.message);
+                    }
+                });
+            }else if(para == 1){
+                if(typeof attr == "object"){
+                    for(var a in attr){
+                        this.setCss(el,a,attr[a]);
+                    }
+                }else if(typeof attr == "string"){
+                    try{
+                        return window.getComputedStyle(el,null)[attr];//ie8+,w3c
+                    }catch(e){
+                        return el.currentStyle(attr);
+                    }
+                }
+            }
+        },
+        insertAfter: function(newNode){
+            var el = this[0];
+            if(el.nextElementSibling){
+                el.parentNode.insertBefore(newNode,el.nextElementSibling);
+            }else{
+                el.parentNode.insertBefore(newNode);
+            }
+        },
+        siblings: function(){
+            var el = this[0];
+            if(el){
+                return this.preSiblings(el).concat(this.nextSiblings(el));
+            }
+            return [];
+        },
+        offset: function(){
+            var el = this[0],
+                l = el.offsetLeft || 0,
+                t = el.offsetTop || 0,
+                p = el.offsetParent;
+            while(p){
+                l += p.offsetLeft;
+                t += p.offsetTop;
+                p = p.offsetParent
+            }
+            return {left:l,top:t};
+        },
+        addEvent: function(type,fn){
+            var el = this[0];
+            if(el.addEventListener){
+                el.addEventListener(type,fn,false);
+            }else if(el.attachEvent){
+                el.attachEvent("on"+type,fn);
+            }
+        },
+        text: function(text){
+            this.each(function(){
+                this.innerHTML = text;
+            });
+        },
+        removeEvent: function(type,fn){
+            var el = this[0];
+            if(el.removeEventListener){
+                el.removeEventListener(type,fn,false);
+            }else if(el.detachEvent){
+                el.detachEvent("on"+type,fn);
+            }
+        },
+        bind: function(type,fn){
+            this.each(function(){
+                if(this.addEventListener){
+                    this.addEventListener(type,fn,false);
+                }else if(this.attachEvent){
+                    this.attachEvent("on"+type,fn);
+                }
+            });
+        },
+        unbind: function(type,fn){
+            this.each(function(){
+                if(this.removeEventListener){
+                    this.removeEventListener(type,fn,false);
+                }else if(this.detachEvent){
+                    this.detachEvent("on"+type,fn);
+                }
+            });
+        }
+    });
 
     window.$ === undefined && (window.$ = $);
 
